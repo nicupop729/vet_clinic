aa80f2ad77b9000203197e0bb4bc3a5d203c2e61
Nicolae Pop <nicupop729@gmail.com>
2022-01-11 19:13:51 -0300

    feat: update the weight of animals;

--- data.sql
+++ data.sql
@@ -52,3 +52,31 @@ ROLLBACK;
 /* After the roll back verify if all records in the animals table still exist.  */
 
 SELECT * FROM animals;
+
+/* Inside a transaction: */
+
+BEGIN;
+
+/* Delete all animals born after Jan 1st, 2022 */
+
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+
+/* Create a savepoint for the transaction */
+
+SAVEPOINT SP1;
+
+/* Update all animals' weight to be their weight multiplied by -1 */
+
+UPDATE animals SET weight_kg = weight_kg * -1;
+
+/* Rollback to the savepoint */
+
+ROLLBACK TO SP1;
+
+/* Update all animals' weights that are negative to be their weight multiplied by -1 */
+
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+
+/* Commit transaction */
+
+COMMIT;
