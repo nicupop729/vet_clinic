01ba07f752576a466d5fe374208792737ae316c2
micheaol <micheaol80@gmail.com>
2022-01-18 20:41:11 +0100

    Add Join table, create JOIN query and add Fkeys contraints

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -37,4 +37,32 @@ CREATE TABLE invoices (
     payed_at TIMESTAMP,
     medical_history_id INT,
     PRIMARY KEY (id)
-)
+);
+
+ALTER TABLE medical_histories
+ADD CONSTRAINT medical_fk FOREIGN KEY (patient_id) REFERENCES patients (id);
+
+ALTER TABLE invoice_items
+ADD CONSTRAINT invoice_fk FOREIGN KEY (invoice_id) REFERENCES invoices (id);
+
+ALTER TABLE invoice_items
+ADD CONSTRAINT treatment_fk FOREIGN KEY (treatment_id) REFERENCES treatments (id);
+
+ALTER TABLE invoices
+ADD CONSTRAINT medical_invoice_fk FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id);
+
+CREATE TABLE medical_history_treatments (
+    id SERIAL PRIMARY KEY,
+    medical_history_id INT,
+    treatment_id INT,
+    CONSTRAINT medical_history_fk
+    FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id),
+    CONSTRAINT treatment_fk
+    FOREIGN KEY (treatment_id) REFERENCES treatments (id)
+);
+
+SELECT * FROM medical_histories
+INNER JOIN medical_history_treatments
+ON medical_histories.id = medical_history_treatments.medical_history_id
+INNER JOIN treatments
+ON medical_history_treatments.treatments_id = treatments.id;
